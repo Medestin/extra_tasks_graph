fa350981c9710036782addfe3522ca2b0c43bee4
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-10-02 13:22:57 +0200

    feat: Finish the Vertex class

--- src/main/java/graph/Vertex.java
+++ src/main/java/graph/Vertex.java
@@ -2,6 +2,7 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Vertex {
     private List<Edge> neigbourhood;
@@ -21,5 +22,39 @@ public class Vertex {
 
     public boolean containsNeighbour(Edge edge){
         return this.neigbourhood.contains(edge);
+    }
+
+    Edge removeNeighbour(int index){
+        return this.neigbourhood.remove(index);
+    }
+
+    public void removeNeighbour(Edge edge){
+        this.neigbourhood.remove(edge);
+    }
+
+    public int getNeighourhoodSize(){
+        return this.neigbourhood.size();
+    }
+
+    public String getLabel(){
+        return this.label;
+    }
+
+    @Override
+    public String toString(){
+        return "Vertex " + this.label;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return Objects.equals(label, vertex.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
     }
 }
