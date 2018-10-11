469526bef923029690b0ca8c40d64bb137376bda
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-10-11 17:16:13 +0200

    feat: BFS & DFS

--- src/main/java/graph/GraphSearch.java
+++ src/main/java/graph/GraphSearch.java
@@ -7,51 +7,109 @@ import java.util.List;
 public final class GraphSearch {
     private static final List<Edge> alreadySearched = new ArrayList<>();
     private static final ArrayDeque<Vertex> searchQueue = new ArrayDeque<>();
+    private static final List<Vertex> result = new ArrayList<>();
 
     public static void bFS(Graph graph, Vertex startingPoint) {
-        alreadySearched.clear();
-        searchQueue.clear();
+        clearCollections();
+
         if (!graph.containsVertex(startingPoint)) {
             System.out.println("Wrong Vertex");
             return;
         }
 
         searchQueue.offer(graph.getVertex(startingPoint.getLabel()));
-        Vertex currentVertex;
-        while(!searchQueue.isEmpty()){
-            currentVertex = searchQueue.pollFirst();
-            searchEdges(currentVertex);
+        while (searchQueue.size() != 0) {
+            bfsSingleSearch();
         }
+    }
+
+    private static void bfsSingleSearch() {
+        Vertex currentVertex = searchQueue.poll();
+
+        for (int i = 0; i < currentVertex.getNeighbourhoodSize(); i++) {
+            Edge currentEdge = currentVertex.getNeighbour(i);
+            if (!isAlreadySearched(currentEdge)) {
+                if (!isNeighbourSearched(currentEdge, currentVertex)) {
+                    searchQueue.offer(currentEdge.getNeighbour(currentVertex));
+                    alreadySearched.add(currentEdge);
+                }
+            }
+        }
+        result.add(currentVertex);
     }
 
     public static void dFS(Graph graph, Vertex startingPoint) {
-        alreadySearched.clear();
-        searchQueue.clear();
+        clearCollections();
+
         if (!graph.containsVertex(startingPoint)) {
             System.out.println("Wrong Vertex");
             return;
         }
 
         searchQueue.offer(graph.getVertex(startingPoint.getLabel()));
-        Vertex currentVertex;
-        while(!searchQueue.isEmpty()){
-            currentVertex = searchQueue.pollLast();
-            searchEdges(currentVertex);
+        while (searchQueue.size() != 0) {
+            dfsSingleSearch();
         }
     }
 
-    private static void searchEdges(Vertex currentVertex) {
-        for (int i = 0; i < currentVertex.getNeighbourhoodSize(); i++) {
-            Edge currentEdge = currentVertex.getNeighbour(i);
-            if (!alreadySearched.contains(currentEdge)) {
-                alreadySearched.add(currentEdge);
-                searchQueue.offerLast(currentEdge.getNeighbour(currentVertex));
-            }
+    private static void dfsSingleSearch(){
+        result.add(searchQueue.poll());
+        int resultIndex = result.size()-1;
+        int neighbourIndex = 0;
+
+        Vertex currentVertex = result.get(resultIndex);
+        Edge currentEdge = currentVertex.getNeighbour(neighbourIndex);
+        int currentNeighbourhoodSize = currentVertex.getNeighbourhoodSize();
+
+        while(isNeighbourSearched(currentEdge, currentVertex)){
+             if(neighbourIndex < currentNeighbourhoodSize-1){
+                 neighbourIndex++;
+                 currentEdge = currentVertex.getNeighbour(neighbourIndex);
+             } else if (resultIndex > 0) {
+                 resultIndex--;
+                 neighbourIndex = 0;
+                 currentVertex = result.get(resultIndex);
+                 currentEdge = currentVertex.getNeighbour(neighbourIndex);
+                 currentNeighbourhoodSize = currentVertex.getNeighbourhoodSize();
+             } else break;
+        }
+
+        if(!isNeighbourSearched(currentEdge, currentVertex)){
+            searchQueue.offer(currentEdge.getNeighbour(currentVertex));
         }
     }
 
-    public static List<Edge> getAlreadySearched(){
+    private static boolean isAlreadySearched(Edge edge) {
+        return alreadySearched.contains(edge);
+    }
+
+    private static boolean isNeighbourSearched(Edge edge, Vertex vertex) {
+        return (isNeighbourInQueue(edge, vertex) || isNeighbourInResult(edge, vertex));
+    }
+
+    private static boolean isNeighbourInQueue(Edge edge, Vertex vertex) {
+        return searchQueue.contains(edge.getNeighbour(vertex));
+    }
+
+    private static boolean isNeighbourInResult(Edge edge, Vertex vertex) {
+        return result.contains(edge.getNeighbour(vertex));
+    }
+
+    private static void clearCollections() {
+        alreadySearched.clear();
+        searchQueue.clear();
+        result.clear();
+    }
+
+    public static List<Edge> getAlreadySearched() {
         return new ArrayList<>(alreadySearched);
     }
 
+    public static List<Vertex> getResult() {
+        return result;
+    }
+
+    public static ArrayDeque<Vertex> getSearchQueue() {
+        return searchQueue;
+    }
 }
--- src/test/java/graph/SearchTestSuite.java
+++ src/test/java/graph/SearchTestSuite.java
@@ -7,43 +7,63 @@ public class SearchTestSuite {
 
     @Before
     public void addSomeVerticesAndEdges() {
-        int numberOfVertices = 10;
-        for (int i = 0; i < numberOfVertices; i++) {
-            graph.addVertex(new Vertex("" + i), true);
-        }
-        Vertex vertexOne;
-        Vertex vertexTwo;
-        for (int i = 0; i < numberOfVertices-1; i++) {
-            vertexOne = graph.getVertex(String.valueOf(i));
-            vertexTwo = graph.getVertex(String.valueOf(i + 1));
-            graph.addEdge(vertexOne, vertexTwo);
-        }
+        Vertex v0 = new Vertex("0");
+        Vertex v11 = new Vertex("11");
+        Vertex v12 = new Vertex("12");
+        Vertex v13 = new Vertex("13");
+        Vertex v21 = new Vertex("21");
+        Vertex v22 = new Vertex("22");
+        Vertex v23 = new Vertex("23");
+        Vertex v24 = new Vertex("24");
+        Vertex v25 = new Vertex("25");
+        Vertex v26 = new Vertex("26");
 
-        int v1 = 0, v2 = 2;
-        for(int i = 0; i < numberOfVertices - 2; i++){
-            vertexOne = graph.getVertex(String.valueOf(v1));
-            vertexTwo = graph.getVertex(String.valueOf(v2));
-            graph.addEdge(vertexOne, vertexTwo);
-        }
+        graph.addVertex(v0, true);
+        graph.addVertex(v11, true);
+        graph.addVertex(v12, true);
+        graph.addVertex(v13, true);
+        graph.addVertex(v21, true);
+        graph.addVertex(v22, true);
+        graph.addVertex(v23, true);
+        graph.addVertex(v24, true);
+        graph.addVertex(v25, true);
+        graph.addVertex(v26, true);
+
+        graph.addEdge(v0, v11);
+        graph.addEdge(v0, v12);
+        graph.addEdge(v0, v13);
+
+        graph.addEdge(v11, v21);
+        graph.addEdge(v11, v22);
+        graph.addEdge(v12, v23);
+        graph.addEdge(v12, v24);
+        graph.addEdge(v13, v25);
+        graph.addEdge(v13, v26);
+
+        graph.addEdge(v21, v23);
+        graph.addEdge(v21, v26);
+        graph.addEdge(v22, v24);
+        graph.addEdge(v22, v25);
+        graph.addEdge(v23, v25);
+        graph.addEdge(v24, v26);
     }
 
     @After
-    public void afterTest(){
+    public void afterTest() {
         System.out.println();
     }
 
     @Test
-    public void testBfs(){
-        GraphSearch.bFS(graph, new Vertex(String.valueOf(0)));
-        System.out.println(GraphSearch.getAlreadySearched());
-        Assert.assertEquals(graph.getEdges().size(), GraphSearch.getAlreadySearched().size());
+    public void testBfs() {
+        GraphSearch.bFS(graph, new Vertex("0"));
+        System.out.println(GraphSearch.getResult());
+        Assert.assertEquals(graph.vertexKeys().size(), GraphSearch.getResult().size());
     }
 
     @Test
-    public void testDfs(){
-        GraphSearch.dFS(graph, new Vertex(String.valueOf(0)));
-        System.out.println(GraphSearch.getAlreadySearched());
-        Assert.assertEquals(graph.getEdges().size(), GraphSearch.getAlreadySearched().size());
+    public void testDfs() {
+        GraphSearch.dFS(graph, new Vertex("0"));
+        System.out.println(GraphSearch.getResult());
+        Assert.assertEquals(graph.vertexKeys().size(), GraphSearch.getResult().size());
     }
-
 }
